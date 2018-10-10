d169fadea9252b59db58faee4186f93c04389e46
Imran <imramuha@student.arteveldehs.be>
2018-10-10 11:57:10 +0200

    first commit

--- labo3-imosh/src/app/services/consume-rest-api.service.ts
+++ labo3-imosh/src/app/services/consume-rest-api.service.ts
@@ -7,7 +7,8 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ConsumeRestApiService {
 
-  private baseUrl = 'http://localhost/wordpress/wp-json/wp/v2';
+  private baseUrl = ' http://localhost/wordpress/wp-json/wp/v2';
+  // Local werkt wel, TODO: https://imramuha.cmsdevelopment.be/wordpress/wp-json/wp/v2
 
   headers: Headers = new Headers;
   options: any;
